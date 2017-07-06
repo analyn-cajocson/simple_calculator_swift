03d4d9a77e399ba8d27eaf04d0c46d9d31a21f9d
analyn <analyncajocson@ymail.com>
2017-07-06 19:11:13 +0800

    with operation

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -11,17 +11,73 @@ import UIKit
 class ViewController: UIViewController {
     
     var numberOnScreen:Double = 0;
+    var previousNumber:Double = 0;
+    var performingMath = false
+    var operation = 0;
 
     @IBOutlet weak var label: UILabel!
     @IBAction func numbers(_ sender: UIButton) {
-        label.text = label.text!  + String(sender.tag-1)
-        numberOnScreen = Double(label.text!)!
+        
+        if performingMath == true {
+            label.text = String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+            performingMath = false
+        } else {
+            label.text = label.text!  + String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+        
+        }
+        
+        
     }
     
     @IBAction func buttons(_ sender: UIButton) {
+        if label.text != "" && sender.tag != 11 && sender.tag != 16 {
+            
+            previousNumber = Double(label.text!)!
+            
+            switch (sender.tag){
+            case 12: // Divide
+                label.text = "/"
+            case 13: // Multiply
+                label.text = "x"
+            case 14: // Minus
+                label.text = "-"
+            case 15: // Add
+                label.text = "+"
+           
+            default:
+                label.text = label.text
+            }
+            
+            operation = sender.tag
+            performingMath = true;
+        
+        } else if sender.tag == 16 {
+            
+            switch (operation ){
+            case 12: // Divide
+                label.text = String(previousNumber / numberOnScreen)
+            case 13: // Multiply
+                label.text = String(previousNumber * numberOnScreen)
+            case 14: // Minus
+                label.text = String(previousNumber - numberOnScreen)
+            case 15: // Add
+                label.text = String(previousNumber + numberOnScreen)
+                
+            default:
+                label.text = label.text
+            }
+        
+        } else if sender.tag == 11 {
+            label.text = "";
+            previousNumber = 0;
+            numberOnScreen = 0;
+            operation = 0;
+        }
     }
     
-     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
